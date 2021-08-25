32014c6f74a0d285c7828bee793b4928e74b8953
Zachary Caldwell <mintyst.john@gmail.com>
2021-08-25 16:35:56 -0700

    done with animals

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 // import functions
 
 // reference needed DOM elements
+const catImage = document.getElementById('cat-image');
 
+const catAudio = document.getElementById('cat-audio');
 
+catImage.addEventListener('click', () => {
+    catAudio.play();
+});
+
+const dogImage = document.getElementById('dog-image');
+
+const dogAudio = document.getElementById('dog-audio');
+
+dogImage.addEventListener('click', () => {
+    dogAudio.play();
+});
+
+const horseImage = document.getElementById('horse-image');
+
+const horseAudio = document.getElementById('horse-audio');
+
+horseImage.addEventListener('click', () => {
+    horseAudio.play();
+});
 // set event listeners 
   // get user input(s)
   // do any needed work with the value(s)
